e355d7ad94b4b948ac78ba3195e11cc0aa114aa1
denishudyakov <denis.hudyakov27@gmail.com>
2024-02-07 20:51:26 +0500

    homework done

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,19 +1,31 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT company_name, last_name, first_name
-FROM customers
-LEFT JOIN employees USING(city)
-WHERE city = 'London'
-
+SELECT customers.company_name, employees.last_name, employees.first_name
+FROM orders
+LEFT JOIN customers USING(customer_id)
+LEFT JOIN employees USING(employee_id)
+LEFT JOIN shippers ON orders.ship_via = shipper_id
+WHERE employees.city = 'London'
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT products.product_name, products.units_in_stock, suppliers.contact_name, suppliers.phone,
+categories.category_name
+FROM products
+LEFT JOIN suppliers USING(supplier_id)
+LEFT JOIN categories USING(category_id)
+WHERE discontinued = 1 AND units_in_stock < 25 AND categories.category_name IN ('Dairy Products', 'Condiments')
+ORDER BY products.units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+WHERE NOT EXISTS (SELECT * FROM orders WHERE customers.customer_id=orders.customer_id)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT product_name
+FROM products
+WHERE product_id IN (SELECT product_id FROM order_details WHERE quantity = 10)
